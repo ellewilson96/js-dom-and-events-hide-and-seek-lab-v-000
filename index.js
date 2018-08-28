b81d59185217ef0eda5abc15e7a6bb2755967565
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-28 13:56:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-function getFirstSelector(selector) {
-  return document.querySelector(selector);
+function getFirstSelector(s) {
+  return document.querySelector(s)
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested .target');
+  return document.querySelector('#nested .target')
 }
-
-function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-    }
-  }
-}
-
 
 function deepestChild() {
   let node = document.getElementById('grand-node')
@@ -30,4 +17,15 @@ function deepestChild() {
 
   return node
 }
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
 }
