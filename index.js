0a914685d13d2e9ca859ce235924d9077622a80a
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-28 13:50:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').querySelectorAll('div');
+  return document.querySelectorAll('div').getElementById('.target').;
 }
